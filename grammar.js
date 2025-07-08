67a17e3f4c4372cfa20b9a27602249ef5c80af0d
Iñaki Amatria Barral <inaki.amatria@appentra.com>
2025-07-08 12:45:03 +0200

    Parse multiline preprocessor comments

--- grammar.js
+++ grammar.js
@@ -68,6 +68,7 @@ module.exports = grammar({
     // preprocessor statements
     /\s|\\\r?\n/,
     $.comment,
+    $.multiline_preproc_comment,
     '&',
   ],
 
@@ -241,7 +242,8 @@ module.exports = grammar({
       ')',
     ),
 
-    preproc_comment: $ => choice(/\/\*.*\*\//, /\/\/.*/),
+    inline_preproc_comment: $ => /\/\/.*/,
+    multiline_preproc_comment: $ => /\/\*([^*]|\*+[^*/])*\*+\//,
 
     preproc_binary_expression: $ => {
       const table = [
@@ -2438,37 +2440,51 @@ function preprocIf(suffix, content, precedence = 0) {
     );
   }
 
+  /**
+    *
+    * @param {GrammarSymbols<string>} $
+    *
+    * @return {ChoiceRule}
+    *
+    */
+  function preprocComment($) {
+    return choice(
+      $.inline_preproc_comment,
+      $.multiline_preproc_comment,
+    );
+  }
+
   return {
     ['preproc_if' + suffix]: $ => prec(precedence, seq(
       preprocessor('if'),
       field('condition', $._preproc_expression),
-      optional($.preproc_comment),
+      optional(preprocComment($)),
       '\n',
       content($),
       field('alternative', optional(alternativeBlock($))),
       preprocessor('endif'),
-      optional($.preproc_comment),
+      optional(preprocComment($)),
     )),
 
     ['preproc_ifdef' + suffix]: $ => prec(precedence, seq(
       choice(preprocessor('ifdef'), preprocessor('ifndef')),
       field('name', $.identifier),
-      optional($.preproc_comment),
+      optional(preprocComment($)),
       content($),
       field('alternative', optional(alternativeBlock($))),
       preprocessor('endif'),
-      optional($.preproc_comment),
+      optional(preprocComment($)),
     )),
 
     ['preproc_else' + suffix]: $ => prec(precedence, seq(
       preprocessor('else'),
-      optional($.preproc_comment),
+      optional(preprocComment($)),
       content($),
     )),
 
     ['preproc_elif' + suffix]: $ => prec(precedence, seq(
       preprocessor('elif'),
-      optional($.preproc_comment),
+      optional(preprocComment($)),
       field('condition', $._preproc_expression),
       '\n',
       content($),
@@ -2478,7 +2494,7 @@ function preprocIf(suffix, content, precedence = 0) {
     ['preproc_elifdef' + suffix]: $ => prec(precedence, seq(
       choice(preprocessor('elifdef'), preprocessor('elifndef')),
       field('name', $.identifier),
-      optional($.preproc_comment),
+      optional(preprocComment($)),
       content($),
       field('alternative', optional(alternativeBlock($))),
     )),
